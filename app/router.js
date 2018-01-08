00c83a89d6c35627f78256f2a0868be74b8c2337
Yuci Gou <ygou@ebi.ac.uk>
2018-01-08 15:52:38 +0000

    Add links

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('scientists');
-  this.route('programmers');
+  this.route('programmers', {path: '/'});
 });
 
 export default Router;
